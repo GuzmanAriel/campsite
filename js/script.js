1949dea459a4b7c7d93f8c8d77ca69e053f406ff
GuzmanAriel <guzmanariel525@gmail.com>
2024-12-27 10:28:43 -0500

    Added in the weather api

--- js/script.js
+++ js/script.js
@@ -1,20 +1,47 @@
-console.log('javascript connected!');
-    
 const carousel = new bootstrap.Carousel('#homeCarousel', {
-    interval: 2000,
+    interval: 5000,
     pause: false
 })
 
-// when the pause button is clicked, pause the carousel
-const carouselPause = document.getElementById('carouselPause');
-carouselPause.addEventListener('click', function() {
-    console.log('pausing the carousel');
-    carousel.pause();
-})
+const carouselButton = document.getElementById('carouselButton');
+const faIcon = document.getElementById('faButton');
 
-// when the play button is clicked, begin cycling through the images
-const carouselPlay = document.getElementById('carouselPlay');
-carouselPlay.addEventListener('click', function() {
-    console.log('cycle the carousel');
-    carousel.cycle();
-})
+carouselButton.addEventListener('click', function () {
+    if (faIcon.classList.contains('fa-pause')) {
+        faIcon.classList.remove('fa-pause');
+        faIcon.classList.add('fa-play');
+        carousel.pause();
+    } else {
+        faIcon.classList.remove('fa-play');
+        faIcon.classList.add('fa-pause');
+        carousel.cycle();
+    }
+});
+
+async function fetchWeather() {
+    const apiKey = process.env.OPEN_WEATHER_API_KEY;
+    const city = "Austin";
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log(data);
+      displayWeather(data)
+    } catch (error) {
+      console.error('There was an error!', error);
+    }
+  }
+  fetchWeather();
+
+  function displayWeather(data) {
+    const icon = document.getElementById("weather-icon");
+    const temp = document.getElementById("weather-temp");
+    const desc = document.getElementById("weather-description");
+    const iconElement = document.createElement("img");
+    iconElement.src = `https://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+    icon.appendChild(iconElement);
+
+    temp.textContent =  `${data.main.temp}\u00B0`;
+    desc.textContent = data.weather[0].description;
+  }
